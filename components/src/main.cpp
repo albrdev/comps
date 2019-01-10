be7cbd143d06d32647541ee77171aa5532e3c4fd
albrdev <albrdev@gmail.com>
2019-01-10 18:23:00 +0100

    input values

--- components/src/main.cpp
+++ components/src/main.cpp
@@ -33,10 +33,8 @@ prefixdata *prefixdata_findbysymbol(const char *const symbol)
 {
     size_t i;
 
-    if(*symbol == '\0')
-    {
+    if(symbol == NULL || *symbol == '\0')
         return &_si_prefix_default;
-    }
 
     for(i = 0U; i < arraycount(_si_prefixes); i++)
     {
@@ -52,6 +50,55 @@ prefixdata *prefixdata_findbysymbol(const char *const symbol)
 int main()
 {
     std::vector<Resistor> resistors;
+    std::string input;
+    bool isParallel;
+
+    std::cout << "Resistors are parallel (y/n)? ";
+    std::getline(std::cin, input);
+    if(input == "y") isParallel = true;
+    else isParallel = false;
+
+    while(true)
+    {
+        char *prefixPart;
+        prefixdata *prefix;
+
+        // Input resistance
+        std::cout << "Resistance: ";
+        std::getline(std::cin, input);
+        if(input.empty()) break;
+        double resistance = strtod(input.c_str(), &prefixPart);
+
+        prefix = prefixdata_findbysymbol(prefixPart);
+        if(prefix != NULL)
+            resistance /= prefix->multiplier;
+
+        // Input tolerance
+        std::cout << "Tolerance: ";
+        std::getline(std::cin, input);
+        if(input.empty()) break;
+        double tolerance = strtod(input.c_str(), &prefixPart);
+
+        prefix = prefixdata_findbysymbol(prefixPart);
+        if(prefix != NULL)
+            tolerance /= prefix->multiplier;
+
+        // Input max. effect
+        std::cout << "Max. effect: ";
+        std::getline(std::cin, input);
+        if(input.empty()) break;
+        double maxEffect = strtod(input.c_str(), &prefixPart);
+
+        prefix = prefixdata_findbysymbol(prefixPart);
+        if(prefix != NULL)
+            maxEffect /= prefix->multiplier;
+
+        // Create a new resistor and put it in the vector
+        resistors.push_back(Resistor(resistance, tolerance, maxEffect));
+    }
+
+    Resistor result = Resistor::Combine(resistors, isParallel);
+    std::cout << "Total resistance: " << result.GetResistance() << std::endl;
 
     return 0;
 }
