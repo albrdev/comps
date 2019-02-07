8b94af652434d4cf5f8e4cbd14ab51f885323e06
albrdev <albrdev@gmail.com>
2019-02-07 01:21:40 +0100

    fix

--- components/src/OR.cpp
+++ components/src/OR.cpp
@@ -1,5 +1,5 @@
 #include "OR.hpp"
 
-bool OR::GetResult(void) const { return m_A && m_B; }
+bool OR::GetResult(void) const { return m_A || m_B; }
 
 OR::OR(const bool a, const bool b, const bool autoID) : LogicGate2x(a, b, autoID) { }
